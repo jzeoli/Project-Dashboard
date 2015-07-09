63baf16b874601486207281eb7b363d12abcd4e6
Joe Zeoli <jzeoli@mam-jzeoli.miles.office>
2015-07-09 08:08:52 -0400

    Added Dates and sorted dashboard

--- client/app/dashboard/main/dashboard.controller.js
+++ client/app/dashboard/main/dashboard.controller.js
@@ -1,29 +1,33 @@
 'use strict';
 
 angular.module('projectDashboardMilesApp')
-  .controller('DashboardCtrl', function ($scope, Project) {
-
-$scope.activeSection = {};
-
-    $scope.projects = Project.query(function(result){
-	for (var project in result){
-		for (var key in result[project].stages){
-			if(result[project].stages[key].isActive){
-				$scope.activeSection = result[project].stages[key];
-				$scope.activeSection.totalTasks = result[project].stages[key].tasks.length;
-			}
-		}
-	}
-	
-	for (var key in  $scope.activeSection.tasks){
-		$scope.activeSection.completedTasks = 0;
-	if($scope.activeSection.tasks[key].isDone){
-		$scope.activeSection.completedTasks++;
-	}
-	}
-	
-	});
-	
+    .controller('DashboardCtrl', function ($scope, Project) {
 
 
-  });
+
+        $scope.projects = Project.query(function (result) {
+            for (var project in result) {
+                for (var key in result[project].stages) {
+                    if (result[project].stages[key].isActive) {
+                        $scope.projects[project].activeSection = result[project].stages[key];
+                        $scope.projects[project].activeSection.totalTasks = result[project].stages[key].tasks.length;
+                    }
+                }
+
+
+               for (var key in $scope.projects[project].activeSection.tasks) {
+                $scope.projects[project].activeSection.completedTasks = 0;
+                if ($scope.projects[project].activeSection.tasks[key].isDone) {
+                    $scope.projects[project].activeSection.completedTasks++;
+                }
+            }
+
+
+            }
+
+
+        });
+
+
+
+    });
